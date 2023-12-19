e124792944daf8b2575433e9f2fe58d6b43b7ccc
Allan Musyoka <musyokaallan96@gmail.com>
2023-12-19 14:52:36 +0300

    Added clients section

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,9 +1,18 @@
+import { clients } from "../constants";
 
 function Clients() {
 
     return(
 
-        <h1>Clients</h1>
+        <section className="w-full flex flex-wrap justify-center items-center gap-[80px] py-[100px]">
+
+            {clients.map((client) => (
+
+                <img src={client.logo} key={client.id} alt="logo" className="w-auto h-auto" />
+
+            ))}
+
+        </section>
 
     )
 }
