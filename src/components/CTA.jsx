3c146729bf05f7a20acc04d9e5947421f2505f2e
Allan Musyoka <musyokaallan96@gmail.com>
2023-12-19 15:01:01 +0300

    Added CTA section

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,10 +1,28 @@
 /* eslint-disable react-refresh/only-export-components */
 
+import Buttons from "./Buttons";
+
 function CTA() {
 
     return(
 
-        <h1>CTA</h1>
+        <section>
+
+            <div className="text-center mb-[40px]">
+
+                <h1 className="text-darkGrayish font-semiBold text-[27px] mb-5">Clipboard for iOS and <br className="block sm:hidden" /> Mac OS</h1>
+
+                <p className="text-grayishBlue font-regular text-[15px]">Available for free on the App Store. Download for Mac or iOS, sync with iCloud and you are ready to start adding to your clipboard.</p>
+
+            </div>
+
+            <div>
+
+                <Buttons />
+
+            </div>
+
+        </section>
         
     )
 }
